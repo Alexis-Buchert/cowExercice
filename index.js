2a286d9601a0a3a611f03ca6689dd93bd464a2be
Alexis <alexis.buchert@hotmail.fr>
2021-09-29 12:14:54 +0100

    Création de la Vache

---
+++ index.js
@@ -0,0 +1,9 @@
+const information = require("./information");
+
+const cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : `Hello I am ${information.name} from ${information.campus} campus`,
+    e : "oO",
+    T : "U",
+}))
---
+++ information.js
@@ -0,0 +1,9 @@
+const personne = {
+    name : "Alexis",
+    campus : "Strasbourg"
+}
+    
+module.exports = {
+    name : personne.name,
+    campus: personne.campus,
+  };
